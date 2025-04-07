a184e79cad103ddcefcbbbd93934d5c13910f318
sukh115 <sukh1151102@gmail.com>
2025-04-07 20:10:16 +0900

    feat : 댓글 좋아요 기능 구현

--- server/src/main/java/nbc/newsfeed/domain/entity/NewsFeedLikeEntity.java
+++ server/src/main/java/nbc/newsfeed/domain/entity/NewsFeedLikeEntity.java
@@ -1,15 +1,7 @@
 package nbc.newsfeed.domain.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-import nbc.newsfeed.domain.repository.newsfeedLike.NewsFeedLikeRepository;
-
-import java.util.Optional;
+import lombok.*;
 
 
 @ToString
@@ -18,32 +10,31 @@ import java.util.Optional;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(
-		name = "news_feed_likes",
-		uniqueConstraints = {
-				@UniqueConstraint(columnNames = {"news_feeds_id", "user_id"})
-		}
+        name = "news_feed_likes",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"news_feeds_id", "user_id"})
+        }
 )
 @Entity
-public class NewsFeedLikeEntity extends TimeBaseEntity{
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+public class NewsFeedLikeEntity extends TimeBaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "news_feeds_id", nullable = false)
-	private NewsFeedEntity newsFeed;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "news_feeds_id", nullable = false)
+    private NewsFeedEntity newsFeed;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id", nullable = false)
-	private UserEntity user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
 
 
-	public static NewsFeedLikeEntity of(NewsFeedEntity newsFeed, UserEntity user) {
-		return NewsFeedLikeEntity.builder()
-				.newsFeed(newsFeed)
-				.user(user)
-				.build();
-	}
-
+    public static NewsFeedLikeEntity of(NewsFeedEntity newsFeed, UserEntity user) {
+        return NewsFeedLikeEntity.builder()
+                .newsFeed(newsFeed)
+                .user(user)
+                .build();
+    }
 
 }
--- server/src/main/java/nbc/newsfeed/domain/service/commentLike/CommentLikeService.java
+++ server/src/main/java/nbc/newsfeed/domain/service/commentLike/CommentLikeService.java
@@ -1,4 +1,42 @@
 package nbc.newsfeed.domain.service.commentLike;
 
+import lombok.RequiredArgsConstructor;
+import nbc.newsfeed.common.error.CustomException;
+import nbc.newsfeed.common.error.ErrorCode;
+import nbc.newsfeed.domain.entity.CommentEntity;
+import nbc.newsfeed.domain.entity.UserEntity;
+import nbc.newsfeed.domain.repository.comment.CommentRepository;
+import nbc.newsfeed.domain.repository.commentLike.CommentLikeRepository;
+import nbc.newsfeed.domain.repository.user.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class CommentLikeService {
+
+    private final CommentLikeRepository commentLikeRepository;
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void toggleLike(Long commentId, Long userId) {
+        CommentEntity commentEntity = commentRepository.findById(commentId)
+                .orElseThrow(() -> new CustomException(ErrorCode.COMMENT_NOT_FOUND));
+        UserEntity user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        commentEntity.toggleLike(user, commentLikeRepository);
+    }
+
+    @Transactional(readOnly = true)
+    public int getLikeCount(Long commentId) {
+        Optional<CommentEntity> commentEntity = Optional.ofNullable(commentRepository.findById(commentId)
+                .orElseThrow(() -> new CustomException(ErrorCode.COMMENT_NOT_FOUND)));
+
+        return commentLikeRepository.countByComment(commentEntity);
+    }
 }
+
