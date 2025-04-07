42da06eced520fc444bef9dc2f52f4f26d82dc7e
hugecookie <phrpp5770@gmail.com>
2025-04-07 17:50:02 +0900

    feat: FriendRequest용 에러메시지 추가

--- server/src/main/java/nbc/newsfeed/common/error/ErrorCode.java
+++ server/src/main/java/nbc/newsfeed/common/error/ErrorCode.java
@@ -19,7 +19,12 @@ public enum ErrorCode {
 
     // ✅ 댓글 관련 에러
     COMMENT_NOT_FOUND( "존재하지 않는 댓글입니다", HttpStatus.NOT_FOUND),
-    UNAUTHORIZED( "해당 댓글에 대한 권한이 없습니다", HttpStatus.UNAUTHORIZED);
+    UNAUTHORIZED( "해당 댓글에 대한 권한이 없습니다", HttpStatus.UNAUTHORIZED),
+
+    // ✅ 친구 요청 관련 에러
+    REQUEST_NOT_FOUND( "존재하지 않는 친구 요청입니다", HttpStatus.NOT_FOUND),
+    ALREADY_REQUESTED( "이미 친구 요청을 보냈습니다", HttpStatus.CONFLICT),
+    INVALID_STATUS( "유효하지 않은 친구 요청 상태입니다", HttpStatus.BAD_REQUEST);
 
     private final String message;
     private final HttpStatus status;
