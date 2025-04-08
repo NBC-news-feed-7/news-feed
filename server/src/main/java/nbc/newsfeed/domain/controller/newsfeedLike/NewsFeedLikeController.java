0d58891f16707ddbbb07d6279dbc86c5871d4a90
sukh115 <sukh1151102@gmail.com>
2025-04-08 19:18:13 +0900

    feat : 좋아요 누른사람 목록 구현

--- server/src/main/java/nbc/newsfeed/domain/controller/newsfeedLike/NewsFeedLikeController.java
+++ server/src/main/java/nbc/newsfeed/domain/controller/newsfeedLike/NewsFeedLikeController.java
@@ -1,10 +1,14 @@
 package nbc.newsfeed.domain.controller.newsfeedLike;
 
 import lombok.RequiredArgsConstructor;
+import nbc.newsfeed.domain.dto.newsfeedLike.LikeUserResponseDto;
 import nbc.newsfeed.domain.service.newsfeedLike.NewsFeedLikeService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,5 +32,13 @@ public class NewsFeedLikeController {
     ) {
         int count = newsFeedLikeService.getLikeCount(newsId);
         return ResponseEntity.ok(count);
+    }
+
+    @GetMapping("/{newsId}/like-users")
+    public ResponseEntity<List<LikeUserResponseDto>> getLikeUsers(
+            @PathVariable Long newsId
+    ) {
+        List<LikeUserResponseDto> likeUsers = newsFeedLikeService.getLikeUsers(newsId);
+        return new ResponseEntity<>(likeUsers, HttpStatus.OK);
     }
 }
