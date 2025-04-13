1bed5ca02f0bca29079421235ee15dd621c79247
raon <raon37910@gmail.com>
2025-04-13 18:20:57 +0900

    feat: 최종

--- server/src/main/java/nbc/newsfeed/common/error/GlobalExceptionHandler.java
+++ server/src/main/java/nbc/newsfeed/common/error/GlobalExceptionHandler.java
@@ -1,31 +1,49 @@
 package nbc.newsfeed.common.error;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    // ✅ 1. CustomException 처리
-    @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorResponseDto> handleCustomException(CustomException ex, HttpServletRequest request) {
-        return ResponseEntity
-                .status(ex.getErrorCode().getStatus())
-                .body(ErrorResponseDto.from(ex.getErrorCode(), request.getRequestURI()));
-    }
+	// ✅ 1. CustomException 처리
+	@ExceptionHandler(CustomException.class)
+	public ResponseEntity<ErrorResponseDto> handleCustomException(CustomException ex, HttpServletRequest request) {
+		return ResponseEntity
+			.status(ex.getErrorCode().getStatus())
+			.body(ErrorResponseDto.from(ex.getErrorCode(), request.getRequestURI()));
+	}
 
-    // ✅ 2. @Valid 검증 실패 처리
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponseDto> handleValidationException(MethodArgumentNotValidException ex,
-                                                                      HttpServletRequest request) {
-        return ResponseEntity
-                .badRequest()
-                .body(ErrorResponseDto.from(ex, request.getRequestURI()));
-    }
+	// ✅ 2. @Valid 검증 실패 처리
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponseDto> handleValidationException(MethodArgumentNotValidException ex,
+		HttpServletRequest request) {
+		return ResponseEntity
+			.badRequest()
+			.body(ErrorResponseDto.from(ex, request.getRequestURI()));
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponseDto> handleInternalException(Exception ex,
+		HttpServletRequest request) {
+		log.error("internal server error: {}", ex.getMessage(), ex);
+
+		return ResponseEntity
+			.badRequest()
+			.body(ErrorResponseDto.from(ErrorCode.INTERNAL_SERVER_ERROR, request.getRequestURI()));
+	}
+	//3명박 이상 들어올시
+	@ExceptionHandler(MaxUploadSizeExceededException.class)
+	public ResponseEntity<ErrorResponseDto> handleMaxSizeException(MaxUploadSizeExceededException ex,
+																   HttpServletRequest request) {
+		return ResponseEntity
+				.badRequest()
+				.body(ErrorResponseDto.from(ex, request.getRequestURI()));
+	}
 }
--- server/src/main/java/nbc/newsfeed/domain/dto/newsfeed/NewsFeedPageResponseDto.java
+++ server/src/main/java/nbc/newsfeed/domain/dto/newsfeed/NewsFeedPageResponseDto.java
@@ -1,47 +1,66 @@
 package nbc.newsfeed.domain.dto.newsfeed;
 
+import java.time.LocalDateTime;
+
 import com.querydsl.core.annotations.QueryProjection;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
 public class NewsFeedPageResponseDto {
 
-    private Long feedId;
-    private Long userId;
-    private String nickName;
-    private String title;
-    private String content;
-    private LocalDateTime updatedAt;
-    private Long likeCount;
-    private Long commentCount;
-    private String thumbnailUrl;
-    private Long viewCount;
+	private static final String BASE_URL = "http://localhost:8080/";
+	private static final String DEFAULT_THUMBNAIL = "https://androidguias.com/wp-content/uploads/2021/10/thumbnail-2-1024x512.jpg.webp";
+	private Long feedId;
+	private Long userId;
+	private String nickName;
+	private String title;
+	private String content;
+	private LocalDateTime updatedAt;
+	private Long likeCount;
+	private Long commentCount;
+	private String thumbnailUrl;
+	private Long viewCount;
 
-    private static final String BASE_URL = "http://localhost:8080/";
-    private static final String DEFAULT_THUMBNAIL = "https://androidguias.com/wp-content/uploads/2021/10/thumbnail-2-1024x512.jpg.webp";
+	@QueryProjection
+	public NewsFeedPageResponseDto(Long feedId, Long userId, String nickName,
+		String title, String content,
+		LocalDateTime updatedAt,
+		Long likeCount, Long commentCount,
+		String thumbnailPath) {
+		this.feedId = feedId;
+		this.userId = userId;
+		this.nickName = nickName;
+		this.title = title;
+		this.content = content;
+		this.updatedAt = updatedAt;
+		this.likeCount = likeCount;
+		this.commentCount = commentCount;
+		this.thumbnailUrl = (thumbnailPath != null)
+			? BASE_URL + thumbnailPath.replace("\\", "/")
+			: DEFAULT_THUMBNAIL;
+	}
 
-    @QueryProjection
-    public NewsFeedPageResponseDto(Long feedId, Long userId, String nickName,
-                                   String title, String content,
-                                   LocalDateTime updatedAt,
-                                   Long likeCount, Long commentCount,
-                                   String thumbnailPath, Long viewCount) {
-        this.feedId = feedId;
-        this.userId = userId;
-        this.nickName = nickName;
-        this.title = title;
-        this.content = content;
-        this.updatedAt = updatedAt;
-        this.likeCount = likeCount;
-        this.commentCount = commentCount;
-        this.thumbnailUrl = (thumbnailPath != null)
-                ? BASE_URL + thumbnailPath.replace("\\", "/")
-                : DEFAULT_THUMBNAIL;
-        this.viewCount = viewCount;
-    }
+	@QueryProjection
+	public NewsFeedPageResponseDto(Long feedId, Long userId, String nickName,
+		String title, String content,
+		LocalDateTime updatedAt,
+		Long likeCount, Long commentCount,
+		String thumbnailPath, Long viewCount) {
+		this.feedId = feedId;
+		this.userId = userId;
+		this.nickName = nickName;
+		this.title = title;
+		this.content = content;
+		this.updatedAt = updatedAt;
+		this.likeCount = likeCount;
+		this.commentCount = commentCount;
+		this.thumbnailUrl = (thumbnailPath != null)
+			? BASE_URL + thumbnailPath.replace("\\", "/")
+			: DEFAULT_THUMBNAIL;
+		this.viewCount = viewCount;
+	}
 }
 
